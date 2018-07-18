0bf3cc5316c9a6b77b5450f2d822811301f98483
Samuel Duga <sam.duga@gmail.com>
2018-07-18 08:37:27 -0500

    added http api, implemented fetch, mocked some data

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -4,8 +4,6 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-/* eslint-disable no-console */
-
 let port = 3000;
 let app = express();
 let compiler = webpack(config);
@@ -19,7 +17,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req,res) {
+  res.json([
+    {'id': 1, 'firstName':'Bob', 'lastName': 'Smith', 'email': 'bob@gmail.com'},
+    {'id': 2, 'firstName':'Tammy', 'lastName': 'Norton', 'email': 'tnorton@yahoo.com'},
+    {'id': 3, 'firstName':'Tina', 'lastName': 'Lee', 'email': 'lee.tina@hotmail.com'}
+  ]);
+});
+
 app.listen(port, function (err) {
-  if (err) console.log(err);
+  if (err) console.log(err); // eslint-disable-line no-console
   else open('http://localhost:' + port);
 });
